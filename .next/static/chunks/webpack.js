332714ad8fbc0801976160486bbf65dd1a319ec6
gustaveH <ghabiman@gmail.com>
2023-10-28 20:42:20 -0400

    minor changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "71aff86bc590e876"; }
+/******/ 		__webpack_require__.h = function() { return "65894604baec8016"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
